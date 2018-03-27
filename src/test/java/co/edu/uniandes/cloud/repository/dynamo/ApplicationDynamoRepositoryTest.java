d2ee07506511289be08fc77bdb95a12f1a1882f7
Felipe Martinez <f.martinez2@uniandes.edu.co>
2018-03-27 17:57:51 -0500

    Ajustar columnas de entidad aplicacion para dynamo

--- src/test/java/co/edu/uniandes/cloud/repository/dynamo/ApplicationDynamoRepositoryTest.java
+++ src/test/java/co/edu/uniandes/cloud/repository/dynamo/ApplicationDynamoRepositoryTest.java
@@ -2,19 +2,24 @@ package co.edu.uniandes.cloud.repository.dynamo;
 
 import co.edu.uniandes.cloud.config.DynamoDBConfig;
 import com.baeldung.spring.data.dynamodb.repository.rule.LocalDbCreationRule;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.ClassRule;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {DynamoDBConfig.class})
+@TestPropertySource(properties = {
+    "amazon.dynamodb.endpoint=http://localhost:8000/",
+    "amazon.aws.accesskey=test1",
+    "amazon.aws.secretkey=test231" })
 public class ApplicationDynamoRepositoryTest {
     @ClassRule
     public static LocalDbCreationRule dynamoDB = new LocalDbCreationRule();
 
+    @Autowired
     private ApplicationDynamoRepository repo;
 
     @Before
@@ -23,5 +28,10 @@ public class ApplicationDynamoRepositoryTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void saveApplication() {
+        Assert.assertNotNull(repo);
     }
 }
