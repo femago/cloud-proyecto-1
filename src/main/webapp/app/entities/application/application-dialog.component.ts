e586be50005c64f81ad1bea6c1990c6d2e4b0f9d
Felipe Martinez <f.martinez2@uniandes.edu.co>
2018-04-10 10:48:35 -0500

    remover consulta con problemas al momento de crear nueva aplicacion

--- src/main/webapp/app/entities/application/application-dialog.component.ts
+++ src/main/webapp/app/entities/application/application-dialog.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager, JhiAlertService, JhiDataUtils } from 'ng-jhipster';
+import {Observable} from 'rxjs/Observable';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {JhiAlertService, JhiDataUtils, JhiEventManager} from 'ng-jhipster';
 
-import { Application } from './application.model';
-import { ApplicationPopupService } from './application-popup.service';
-import { ApplicationService } from './application.service';
-import { Contest, ContestService } from '../contest';
+import {Application} from './application.model';
+import {ApplicationPopupService} from './application-popup.service';
+import {ApplicationService} from './application.service';
+import {Contest, ContestService} from '../contest';
 
 @Component({
     selector: 'jhi-application-dialog',
@@ -36,8 +36,8 @@ export class ApplicationDialogComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.isSaving = false;
         this.contest = this.applicationService.currentContest;
-        this.contestService.queryPublished()
-            .subscribe((res: HttpResponse<Contest[]>) => { this.contests = res.body; }, (res: HttpErrorResponse) => this.onError(res.message));
+        // this.contestService.queryPublished()
+        //     .subscribe((res: HttpResponse<Contest[]>) => { this.contests = res.body; }, (res: HttpErrorResponse) => this.onError(res.message));
     }
 
     ngOnDestroy(): void {
